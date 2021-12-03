89b6016078752b53c075525a03185d3e1d7ada14
Agung Zon Blade <53517318+zonblade@users.noreply.github.com>
2021-12-03 19:45:05 +0700

    Update main.js
    perbaikan indensasi

--- fundamental/cari_ganjil_nonPrima/main.js
+++ fundamental/cari_ganjil_nonPrima/main.js
@@ -1,25 +1,25 @@
 const primeGanjilCheck = (angka) => {
-		var find = 0;
-		var result = false;
-		for (var x = 0; x <= angka; x++) {
-			var isInt = angka/x;
-			if (Number.isInteger(isInt)) {
-				find += 1;
+	var find = 0;
+	var result = false;
+	for (var x = 0; x <= angka; x++) {
+		var isInt = angka/x;
+		if (Number.isInteger(isInt)) {
+			find += 1;
+		}
+	}
+	if (find > 2 || find < 2) { result = true; }
+	return result;
+}
+const findPrimeGanjilList = () =>{
+	var hasil = '';
+	for(var x = 0;x<50;x++){
+		if(primeGanjilCheck(x)){
+			if(!Number.isInteger(x/2)){
+				hasil = `${hasil},${x}`
 			}
 		}
-		if (find > 2 || find < 2) { result = true; }
-		return result;
 	}
-	const findPrimeGanjilList = () =>{
-		var hasil = '';
-		for(var x = 0;x<50;x++){
-			if(primeGanjilCheck(x)){
-				if(!Number.isInteger(x/2)){
-					hasil = `${hasil},${x}`
-				}
-			}
-		}
-		hasil = hasil.substring(1);
-		return hasil;
-	}
-  console.log(findPrimeGanjilList())
+	hasil = hasil.substring(1);
+	return hasil;
+}
+console.log(findPrimeGanjilList())
